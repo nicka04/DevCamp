4ad64879bc34c0a30d4ec1eac4b5e86830fea9a2
nicholas.ada@otthydromet.com <nicholas.ada@otthydromet.com>
2023-07-30 13:53:18 -0400

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Blog data"
+    )
+end
+  
+puts "10 blog posts created"
+  
+5.times do |skill|
+    Skill.create!(
+        title: "Skill #{skill}",
+        percent: 10
+     )
+end
+  
+puts "10 blog posts created"
+  
+9.times do |portfolio|
+      Portfolio.create!(
+          title: "portfolio #{portfolio}",
+          subtitle: "subtitle",
+          body: "body text",
+          main_image: "https://placehold.co/600x400",
+          thumb_image: "https://placehold.co/350x200"
+      )
+end
+puts "9 portfolioscreated"
