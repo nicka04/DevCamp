edbe48acd9956cc0b04dd2f95445a17bbe66cd5d
nicholas.ada@otthydromet.com <nicholas.ada@otthydromet.com>
2023-07-31 22:57:21 -0400

    update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[ show edit update destroy ]
     def index
         @portfolios = Portfolio.all
     end
@@ -6,7 +7,10 @@ class PortfoliosController < ApplicationController
     def new
         @portfolio = Portfolio.new
     end
-
+    
+    def edit
+    end
+    
     def create
         
         @portfolio = Portfolio.new(portfolio_params)
@@ -21,6 +25,20 @@ class PortfoliosController < ApplicationController
           end
         end
       end
+ # PATCH/PUT /blogs/1 or /blogs/1.json
+    def update
+      respond_to do |format|
+        if @portfolio.update(portfolio_params)
+          format.html { redirect_to portfolio_path(@portfolio), notice: "Portfolio was successfully updated." }
+          format.json { render :show, status: :ok, location: @portfolio }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+      
+
       private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
@@ -29,6 +47,6 @@ class PortfoliosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
         def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image)
+      params.require(:portfolio).permit(:title, :subtitle, :body)
     end
 end
