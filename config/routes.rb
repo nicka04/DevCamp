cfa73c791878ad1f223e8dbde47c0250125054e9
nicholas.ada@otthydromet.com <nicholas.ada@otthydromet.com>
2023-08-02 00:01:47 -0400

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 
   # Defines the root path route ("/")
   # root "articles#index"
